0acd5c0d45a27ee6032869722aaa553d642861b8
Douglas Cristian <dougjr07@gmail.com>
2024-04-25 22:24:15 -0300

    Criando EventosDTO

---
+++ BackEnd/DTO/EventosDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BackEnd.DTO
+{
+    public class EventosDTO
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public DateTime DataHora { get; set; }
+        public ProjetoDTO Projeto { get; set; }
+        public List<UsuarioDTO>? UsuariosAtribuidos { get; set; }
+    }
+}
